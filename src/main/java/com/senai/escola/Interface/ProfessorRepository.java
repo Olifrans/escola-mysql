21ded48d8fab321ad8a0a928db91fe12ab6b8f3a
Francisco Oliveira <fransoliveira@gmail.com>
2025-08-28 21:42:54 -0300

    java

--- src/main/java/com/senai/escola/Interface/ProfessorRepository.java
+++ src/main/java/com/senai/escola/Interface/ProfessorRepository.java
@@ -1,6 +1,6 @@
 package com.senai.escola.Interface;
 
-import com.senai.escola.Models.Aluno;
+
 import com.senai.escola.Models.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
